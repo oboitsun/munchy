76b151426610cca8e24c86c01ac81a413849d573
Oleksandr Boitsun <thewip3out@gmail.com>
2021-10-22 21:08:06 +0300

    fc

--- pages/components/Package.js
+++ pages/components/Package.js
@@ -2,7 +2,6 @@ import React, { useState, useRef } from "react";
 import Button from "./Button";
 import { motion } from "framer-motion";
 export default function Package({ details }) {
-  const [expanded, setExpanded] = useState(false);
   const [setActive, setActiveState] = useState("");
   const [setHeight, setHeightState] = useState("0px");
 
@@ -15,12 +14,12 @@ export default function Package({ details }) {
   return (
     <div
       className={`mb-10  rounded-[10px] h-auto self-start  overflow-hidden text-white bg-white  ${
-        details.custom ? " shdw-grey" : "shdw-crmsn"
+        details?.custom ? " shdw-grey" : "shdw-crmsn"
       }`}
     >
       <div
         className={`${
-          details.custom ? "bg-drkGray" : "bg-crmsn"
+          details?.custom ? "bg-drkGray" : "bg-crmsn"
         } px-[30px] py-6  rounded-b-[10px] uppercase flex items-center justify-between`}
       >
         <p className="font-extrabold text-xs pr-2">{details.name}</p>
@@ -28,12 +27,12 @@ export default function Package({ details }) {
           <p className="font-bold text-[32px] leading-none">${details.price} </p>
           <p className=" text-xs leading-none ml-2 max-w-min   self-center ">
             +GST/ <br />
-            {!details.custom && `Month`}
+            {!details?.custom ? `Month` : ""}
           </p>
         </div>
       </div>
       <div className="p-[30px] text-grey">
-        {details.custom ? (
+        {details?.custom ? (
           <p className="text-xs leading-normal text-grey">{details.features[0]}</p>
         ) : (
           <>
@@ -65,16 +64,17 @@ export default function Package({ details }) {
               style={{ maxHeight: `${setHeight}` }}
               className="list-disc list-outside pl-5 text-xs leading-normal transition-all overflow-hidden "
             >
-              {details?.adminPanelFeatures.map((feature, i) => (
-                <li
-                  className={`"pl-px ${
-                    i < details.adminPanelFeatures.length - 1 ? "pb-2" : "pb-6"
-                  } `}
-                  key={i}
-                >
-                  {feature}
-                </li>
-              ))}
+              {details?.adminPanelFeatures &&
+                details?.adminPanelFeatures.map((feature, i) => (
+                  <li
+                    className={`"pl-px ${
+                      i < details.adminPanelFeatures.length - 1 ? "pb-2" : "pb-6"
+                    } `}
+                    key={i}
+                  >
+                    {feature}
+                  </li>
+                ))}
             </ul>
 
             <div className="h-px w-full bg-gray-200 box "></div>
@@ -85,7 +85,7 @@ export default function Package({ details }) {
           full
           text="choose plan"
           addClass={
-            details.custom
+            details?.custom
               ? "mt-6 border-2 border-crmsn bg-white text-crmsn box-content hover:bg-crmsn hover:text-white transition-all duration-500"
               : "mt-6 border-2 border-crmsn text-white box-content transition-all duration-500 hover:bg-white hover:text-crmsn"
           }
